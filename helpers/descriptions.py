11338da877775629ff8f8b259713581b85a164a6
ExtraRandom <crazyblob11@gmail.com>
2016-11-07 02:48:33 +0000

    Helpers for Cog Test
    Will probably need to changes these in the near future

---
+++ helpers/descriptions.py
@@ -0,0 +1,59 @@
+"""
+Description strings
+"""
+
+
+main = """RandomBot
+
+Source code: https://github.com/ExtraRandom/Discord-Bot
+
+Some commands have subcommands, type !help command subcommand
+For example: !help steam status"""
+
+"""
+HIDDEN COMMANDS
+"""
+
+isdev = "Only Extra_Random can use this command"
+
+
+"""
+Utility commands
+"""
+
+time = "Check local times over the world"
+time_advanced = "Checks local time over the world and their UTC positions"
+time_sydney = "Checks local time in Sydney and it's UTC position"
+time_london = "Checks local time in London and it's UTC position"
+time_ny = "Checks local time in New York and it's UTC position"
+time_sf = "Checks local time in San Francisco and it's UTC position"
+time_perth = "Checks local time in Perth and its UTC position"
+
+
+"""
+GAMES
+"""
+
+csgo = "Get info on CS:GO Matchmaking"
+
+mc_ip = "Get Status of given Minecraft Server"
+mc_status = "Check Status of Minecraft Servers"
+
+release_datesb = "List of games and countdown until their releases"
+steam = "Commands related to steam"
+steam_status = "Check Status of Steam Servers"
+steam_bs = "List of Top 5 Best selling steam games"
+steam_sales = "PM's you a list of games currently on sale on Steam"
+
+ow = """Find Overwatch Stats of a given battle tag in a given region
+
+Example: !overwatch eu ExtraRandom#2501
+"""
+owb = "Find Overwatch Stats for a battle tag"
+
+
+"""
+CURRENCY
+"""
+
+cc = "Convert currencies!"
---
+++ helpers/tokens_template.py
@@ -0,0 +1,19 @@
+""""""
+
+"""
+Found here: https://discordapp.com/developers/applications/me
+The token for the Discord Bot, Will require creating an Application.
+The token is found under "APP BOT USER" and then "Token: "
+"""
+token = ""
+
+
+
+"""
+Steam Web-API key
+Sign into steam website and get the key from here https://steamcommunity.com/dev/apikey
+
+If you don't wish to use the CS:GO command that relies on this, leave it as just ""
+"""
+
+wa_key = ""
