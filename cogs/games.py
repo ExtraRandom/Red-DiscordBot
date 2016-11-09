0de142f335525f3407ad1312797b9624bbf2c5bd
ExtraRandom <crazyblob11@gmail.com>
2016-11-09 08:59:00 +0000

    PD2 testing
    Working on pd2 stats command

--- cogs/games.py
+++ cogs/games.py
@@ -2,14 +2,14 @@ import asyncio
 import logging
 import bs4
 import requests
-import os
 
-from helpers import descriptions as desc, tokens as t
+from helpers import descriptions as desc, tokens as t, steam_json
 
 import aiohttp
 from discord.ext import commands
 
 from mcstatus import MinecraftServer
+
 
 loop = asyncio.get_event_loop()
 log = logging.getLogger(__name__)
@@ -56,6 +56,129 @@ Description: {}
             await self.bot.say("Server didn't return any info or an error occured.")
             log.warning("Exception in games.py - {}".format(e))
 
+    @commands.command(pass_context=True, description="", brief="")
+    async def pd2(self, ctx):
+        run = True
+        user = str(ctx.message.author)
+        # print(user)
+
+        user_id = steam_json.read(user)
+
+        if not t.web_api == "" and run:
+            try:
+                link = "http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=218620&key={}" \
+                       "&steamid={}&format=json" \
+                       "".format(t.web_api, user_id)
+
+                with aiohttp.ClientSession() as session:
+                    async with session.get(link)as resp:
+                        data = await resp.text()  # resp.json()
+
+                        # TODO add possible error catching
+                        # TODO finish command
+
+                        var = steam_json.test_json(data)
+
+                        heist_s = steam_json.steam_read(data, "heist_success")
+                        heist_f = steam_json.steam_read(data, "heist_failed")
+
+                        diff_norm = steam_json.steam_read(data, "difficulty_normal")
+                        diff_h = steam_json.steam_read(data, "difficulty_hard")
+                        diff_vh = steam_json.steam_read(data, "difficulty_overkill")
+                        diff_ovk = steam_json.steam_read(data, "difficulty_overkill_145")
+                        diff_dw = steam_json.steam_read(data, "difficulty_overkill_290")
+                        diff_my = steam_json.steam_read(data, "difficulty_easy_wish")
+                        diff_od = steam_json.steam_read(data, "difficulty_sm_wish")
+
+                        norm_vh_diff = diff_norm + diff_h + diff_vh
+                        ovk_may_diff = diff_ovk + diff_my
+                        dw_od_diff = diff_dw + diff_od
+
+                        kills_cop = steam_json.steam_read(data, "enemy_kills_cop")
+                        kills_fbi = steam_json.steam_read(data, "enemy_kills_fbi")
+                        kills_fbi_swat = steam_json.steam_read(data, "enemy_kills_fbi_swat")
+                        kills_fbi_heavy_swat = steam_json.steam_read(data, "enemy_kills_fbi_heavy_swat")
+                        kills_swat = steam_json.steam_read(data, "enemy_kills_swat")
+                        kills_heavy_swat = steam_json.steam_read(data, "enemy_kills_heavy_swat")
+                        kills_city_swat = steam_json.steam_read(data, "enemy_kills_city_swat")
+                        kills_security = steam_json.steam_read(data, "enemy_kills_security")
+                        kills_gensec = steam_json.steam_read(data, "enemy_kills_gensec")
+                        kills_gangster = steam_json.steam_read(data, "enemy_kills_gangster")
+                        kills_sniper = steam_json.steam_read(data, "enemy_kills_sniper")
+                        kills_shield = steam_json.steam_read(data, "enemy_kills_shield")
+                        kills_cloaker = steam_json.steam_read(data, "enemy_kills_spooc")
+                        kills_tank = steam_json.steam_read(data, "enemy_kills_tank")
+                        kills_taser = steam_json.steam_read(data, "enemy_kills_taser")
+                        kills_mobster = steam_json.steam_read(data, "enemy_kills_mobster")
+                        kills_mobster_boss = steam_json.steam_read(data, "enemy_kills_mobster_boss")
+                        kills_civilian = steam_json.steam_read(data, "enemy_kills_civilian")
+                        kills_civilian_female = steam_json.steam_read(data, "enemy_kills_civilian")
+                        kills_tank_hw = steam_json.steam_read(data, "enemy_kills_tank_hw")
+                        kills_hector_boss = steam_json.steam_read(data, "enemy_kills_hector_boss")
+                        kills_hector_boss_no_armor = steam_json.steam_read(data, "enemy_kills_hector_boss_no_armor")
+                        kills_tank_green = steam_json.steam_read(data, "enemy_kills_tank_green")
+                        kills_tank_black = steam_json.steam_read(data, "enemy_kills_tank_black")
+                        kills_tank_skull = steam_json.steam_read(data, "enemy_kills_tank_skull")
+                        kills_hostage_rescue = steam_json.steam_read(data, "enemy_kills_hostage_rescue")
+                        kills_murkywater = steam_json.steam_read(data, "enemy_kills_murkywater")
+                        kills_winters_minion = steam_json.steam_read(data, "enemy_kills_phalanx_minion")
+                        kills_biker_boss = steam_json.steam_read(data, "enemy_kills_biker_boss")
+                        kills_cop_female = steam_json.steam_read(data, "enemy_kills_cop_female")
+                        kills_medic = steam_json.steam_read(data, "enemy_kills_medic")
+                        kills_chavez_boss = steam_json.steam_read(data, "enemy_kills_chavez_boss")
+
+                        total_kills = kills_cop + kills_fbi + kills_fbi_swat + kills_fbi_heavy_swat + kills_swat +\
+                            kills_heavy_swat + kills_city_swat + kills_security + kills_gensec + kills_gangster +\
+                            kills_sniper + kills_shield + kills_cloaker + kills_tank + kills_taser + kills_mobster +\
+                            kills_mobster_boss + kills_civilian + kills_civilian_female + kills_tank_hw +\
+                            kills_hector_boss + kills_hector_boss_no_armor + kills_tank_green + kills_tank_black +\
+                            kills_tank_skull + kills_hostage_rescue + kills_murkywater + kills_winters_minion +\
+                            kills_biker_boss + kills_cop_female + kills_medic + kills_chavez_boss
+
+                        tank_kills = kills_tank + kills_tank_skull + kills_tank_green + kills_tank_hw + kills_tank_black
+                        civ_kills = kills_civilian + kills_civilian_female
+                        gang_mob_kills = kills_gangster + kills_mobster_boss + kills_mobster
+
+                        cop_swat = kills_cop + kills_cop_female + kills_city_swat + kills_heavy_swat + kills_swat
+                        fbi = kills_fbi + kills_fbi_heavy_swat + kills_fbi_swat
+
+                        shield_sniper_cloaker = kills_sniper + kills_shield + kills_cloaker
+                        other_kills = total_kills - (tank_kills + gang_mob_kills + civ_kills + shield_sniper_cloaker +
+                                                     cop_swat + fbi)
+
+                        most_used_gun, most_used_kills = steam_json.weapon_read(data)
+
+                        msg = """PD2 Stats for {}:
+
+Heists:           *{}W / {}L*
+Difficulty:      *{} Normal-VH,
+                       {} Overkill-Mayhem,
+                       {} DW+*
+
+Kills:              *FBI {},  Cop/SWAT {},
+                       Shield {},  Sniper {},  Cloaker {}
+                       Bulldozers {},  Gang/Mob {},
+                       Civilian {},  Other {}
+                       Favourite Gun: {} - {} kills*
+                        """.format(user, heist_s, heist_f, norm_vh_diff, ovk_may_diff, dw_od_diff,
+                                   fbi, cop_swat, kills_shield, kills_sniper, kills_cloaker, tank_kills, gang_mob_kills,
+                                   civ_kills, other_kills, most_used_gun, most_used_kills)
+
+                        await self.bot.say(msg)
+
+            except KeyError as e:
+                log.warn("KeyError: {}".format(e))
+                await self.bot.say("Error: Your tag was not found on list. Use `!addsteam` to add it.")
+                # TODO write the addsteam command
+
+            # except Exception as e:
+            #    print(e)
+            #     await self.bot.say("Error getting data - Ask Owner to check WebAPI key is correct")
+            #    log.warn("Invalid WebAPI key")
+        else:
+            await self.bot.say("This command is disabled currently. Ask the bot owner to add a Steam WebAPI key in "
+                               "tokens.py for it to be enabled")
+
     @commands.command(description=desc.csgo, brief=desc.csgo)
     async def csgo(self):
         if not t.web_api == "":
@@ -66,9 +189,6 @@ Description: {}
                 with aiohttp.ClientSession() as session:
                     async with session.get(link)as resp:
                         data = await resp.json()
-
-                        test = "hi"
-                        test.capitalize()
 
                         scheduler = data['result']['matchmaking']['scheduler']
                         servers = data['result']['matchmaking']['online_servers']
---
+++ helpers/steam_json.py
@@ -0,0 +1,93 @@
+import json
+
+file = "helpers/steam_id.json"
+pd2_file = "helpers/pd2_weapons.json"
+
+"""
+For reading and writing to steam_id.json
+"""
+
+
+def read(user):
+    with open(file) as data_file:
+        data = json.load(data_file)
+        # TODO check user exists
+        return data[user]
+
+
+def write(user):
+
+    return 0
+
+    # todo get this code working
+
+    data = read(user)
+
+    with open(file) as out_file:
+        data = json.dump(out_file)
+
+
+"""
+For reading the .json containing stat info
+"""
+
+
+def steam_read(data, stat_name):
+    jdata = json.loads(data)
+    stats = len(jdata['playerstats']['stats'])
+
+    # print(stats)
+
+    for index in range(stats):
+        if jdata['playerstats']['stats'][index]['name'] == stat_name:
+            return jdata['playerstats']['stats'][index]['value']
+    # TODO make log
+    print("No result found for {}. Counting as zero.".format(stat_name))
+    return 0
+
+
+def weapon_read(data):
+    # http://wiki.modworkshop.net/Payday_2/Weapon_IDs
+    jdata = json.loads(data)
+
+    with open(pd2_file) as out_file2:
+        data = json.dump(out_file2)
+
+
+
+    stats = len(jdata['playerstats']['stats'])
+
+    highest_kills = 0
+    highest_gun = ""
+
+    # print(stats)
+
+    for index in range(stats):
+        if str(jdata['playerstats']['stats'][index]['name']).startswith('weapon_kills_'):
+            if int(jdata['playerstats']['stats'][index]['value']) > highest_kills:
+                highest_kills = int(jdata['playerstats']['stats'][index]['value'])
+                highest_gun = str(jdata['playerstats']['stats'][index]['name'])
+
+    if highest_gun == "" or highest_kills == 0:
+        return "N/A", "N/A"
+    else:
+        return highest_gun, highest_kills
+
+
+def test_json(data):
+
+    jdata = json.loads(data)
+    stats = len(jdata['playerstats']['stats'])
+
+    count = 0
+
+    for index in range(stats):
+        if str(jdata['playerstats']['stats'][index]['name']).startswith('weapon_kills_'):
+            count += 1
+            # print(str(jdata['playerstats']['stats'][index]['name']))
+
+    # print(count)
+    return count
+
+
+
