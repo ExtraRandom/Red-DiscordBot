074072f207844505d4533e474ff32db396cb4e7b
ExtraRandom <crazyblob11@gmail.com>
2017-09-26 14:25:44 +0100

    Formatting on Minecraft server description -It's bad I know I'll sort it out somewhen else

--- cogs/games.py
+++ cogs/games.py
@@ -37,6 +37,12 @@ class Games:
             except TypeError:
                 s_desc = data['description']
 
+            s_desc = str(s_desc).replace("§0", "").replace("§1", "").replace("§2", "").replace("§3", "")\
+                .replace("§4", "").replace("§5", "").replace("§6", "").replace("§7", "").replace("§8", "")\
+                .replace("§9", "").replace("§a", "").replace("§b", "").replace("§c", "").replace("§d", "")\
+                .replace("§e", "").replace("§f", "").replace("§k", "").replace("§l", "").replace("§m", "")\
+                .replace("§n", "").replace("§o", "").replace("§r", "").replace("  ", "").replace("  ", "")
+
             player_count = int(data['players']['online'])
             player_limit = int(data['players']['max'])
 
